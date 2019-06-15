8e7321d4fae152064ec79ada8d4a5b86dec03974
VALERE91 <valereplantevin@gmail.com>
2019-06-15 15:42:53 -0400

    Implement characteristic

--- d20tempest/include/components/characteristic.hpp
+++ d20tempest/include/components/characteristic.hpp
@@ -1,27 +1,81 @@
 #pragma once
 
+#include <filesystem>
 #include <string>
 #include <type_traits>
 
+#include <sol3/sol.hpp>
+
 namespace d20tempest::components
 {
-    template<typename T, 
-        typename std::enable_if_t<std::is_trivially_constructible<T>::type || 
-                                  std::is_arithmetic<T>::type> = 0>
-    class Characteristic
+    // the partial specialization of A is enabled via a template parameter
+    template<class T, class Enable = void>
+    class Characteristic {}; // primary template
+
+    template<typename T>
+    class Characteristic<T, typename std::enable_if<std::is_arithmetic<T>::value>::type>
     {
     private:
-        T m_charac;
+        T m_characteristicValue = 0;
+        mutable T m_modifierCache = 0;
+        mutable bool m_cacheInvalidated = true;
+
         std::string m_name;
         std::string m_shortName;
-    
-    public:
-        virtual T Modifier() const = 0;
-        
-        virtual T Value() const = 0;
-        virtual void Value(T value) = 0;
+        std::string m_scriptPath;
 
-        virtual std::string Name() = 0;
-        virtual std::string ShortName() = 0;
+        sol::state m_luaState;
+        sol::function m_getModifier;
+        sol::function m_checkValueBeforeSet;
+    public:
+        explicit Characteristic(const std::string& scriptPath) : m_scriptPath(scriptPath)
+        {
+            m_luaState.script_file(scriptPath);
+            m_getModifier = m_luaState["getModifier"];
+            m_checkValueBeforeSet = m_luaState["checkValueBeforeSet"];
+
+            m_name = m_luaState["name"];
+            m_shortName = m_luaState["shortName"];
+            m_luaState.set_function(m_shortName, [this]{ return Modifier(); });
+            m_luaState.set_function(m_name, [this]{ return Value(); });
+        }
+
+        virtual T Modifier() const
+        {
+            if(m_cacheInvalidated)
+            {
+                m_modifierCache = m_getModifier();
+                m_cacheInvalidated = false;
+            }
+
+            return m_modifierCache;
+        }
+        
+        virtual T Value() const
+        {
+            return m_characteristicValue;
+        }
+
+        virtual bool Value(T value)
+        {
+            if(m_checkValueBeforeSet.valid() && m_checkValueBeforeSet(value))
+            {
+                m_characteristicValue = value;
+                m_cacheInvalidated = true;
+                return true;
+            }
+
+            return false;
+        }
+
+        virtual std::string Name() const
+        {
+            return m_name;
+        }
+
+        virtual std::string ShortName() const
+        {
+            return m_shortName;
+        }
     };
 }
--- d20tempest/src/main.cpp
+++ d20tempest/src/main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <cassert>
+#include <string>
 
 #include "communication/tcp_server.hpp"
 
-#include <sol3/sol.hpp>
-
 int main(int argc, char* argv[])
 {
-    sol::state lua;
-    int x = 0;
-    lua.set_function("beep", [&x]{ ++x; });
-    lua.script("beep()");
-    assert(x == 1);
-
-    d20tempest::communication::TCPServer server("127.0.0.1", 8888);
+    d20tempest::communication::TCPServer server("0.0.0.0", 7777);
     std::cout << "Server launched" << std::endl;
     std::cin.ignore();
 }

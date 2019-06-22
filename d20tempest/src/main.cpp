151fb978d6345252c1f82483e93a32d8dac75c38
VALERE91 <valereplantevin@gmail.com>
2019-06-22 00:57:28 -0400

    Add factory to the main in order for her to listen ClientEvent

--- d20tempest/src/main.cpp
+++ d20tempest/src/main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include <cassert>
-#include <chrono>
 #include <string>
 
-#include <filesystem>
-namespace fs = std::filesystem;
-
 #include "communication/tcp_server.hpp"
+#include "characters/character_factory.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -19,6 +15,7 @@ int main(int argc, char* argv[])
     // character->AddAbility("wisdom", 14);
     // character->AddAbility("charism", 20);
    
+    d20tempest::character::CharacterFactory factory;
     d20tempest::communication::TCPServer server("0.0.0.0", 7777);
     std::cout << "Server launched" << std::endl;
 
